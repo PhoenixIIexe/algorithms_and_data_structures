986a6c7438d3d7c3f7916027477bde9a1315164e
PhoenixIIexe <77460999+PhoenixIIexe@users.noreply.github.com>
2021-06-26 12:52:31 +0300

    Delete lesson_one directory

--- lesson_one/one.py
+++
@@ -1,7 +0,0 @@
-num = int(input())
-
-sums = num % 10 + num // 10 % 10 + num // 100 % 10
-muls = (num % 10) * (num // 10 % 10) * (num // 100 % 10)
-
-print(f'Произведение {muls}')
-print(f'Сумма {sums}')
--- lesson_one/six.py
+++
@@ -1,6 +0,0 @@
-year = int(input('Введите год: '))
-
-if (year % 100 != 0 and year % 4 == 0) or (year % 100 == 0 and year % 400 == 0):
-    print('Год весокосный')
-else:
-    print('Год не весокосный')
